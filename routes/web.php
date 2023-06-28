c6ffec78c6ba3492d016405137f527d8d85f71d9
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-06-28 21:45:36 +0100

    creazione controller e model, recupero dati DB

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $links = config('store.someLinks');
-    return view('welcome', compact('links'));
-});
+use App\Http\Controllers\Guest\PageController as PageController;
 
-Route::get('/otherpage', function () {
-    $links = config('store.someLinks');
-    return view('other', compact('links'));
-});
+Route::get('/', [PageController::class, 'index']);
